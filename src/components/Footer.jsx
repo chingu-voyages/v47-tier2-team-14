6fb5dbba55d1d190df9a4d91ebf2ef537fd43119
Nisha <nishavijai@gmail.com>
2024-01-18 11:31:37 +0100

    Update footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
 import './Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <p>
+        <a
+          href="https://github.com/chingu-voyages/v47-tier2-team-14"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tier2-Team-14 github repository
+        </a>
+      </p>
+    </footer>
   )
 }
 
