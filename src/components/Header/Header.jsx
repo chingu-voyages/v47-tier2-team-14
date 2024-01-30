6b04111a98677225273e7216e71dc5b8ef6bd5ab
Austine <austineuwumwonse@gmail.com>
2024-01-30 13:20:02 +0100

    added navbar
    no functionality added yet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
-import styles from './Header.module.css';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-      <h1 className={styles['header-text']}>Chingu Task Manager</h1>
+    <header className={styles.header}>
+      <div className={styles["search-bar"]}>
+        <input type="text" placeholder="Search..." />
+        <button>
+          <SearchIcon />
+        </button>
+      </div>
+      <button className={styles["notifications-button"]}>
+        <NotificationsIcon />
+      </button>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,36 +1,54 @@
-import styles from './SideMenu.module.css';
+import styles from "./SideMenu.module.css";
 
-import ShowTasks from '../ShowTasks/ShowTasks';
+import ShowTasks from "../ShowTasks/ShowTasks";
 
-import { useGetRoutineActivitiesQuery, useGetStudyingQuery, useGetDailyTasksProjectsQuery, useGetChinguQuery } from '../../redux/api.js';
+import {
+  useGetRoutineActivitiesQuery,
+  useGetStudyingQuery,
+  useGetDailyTasksProjectsQuery,
+  useGetChinguQuery,
+} from "../../redux/api.js";
 
 const SideMenu = () => {
-    const { data: routineActivityQuery, isSuccess: isRoutineActivityQuerySuccess } = useGetRoutineActivitiesQuery();
-    const { data: studyingQuery, isSuccess: isStudyingQuerySuccess } = useGetStudyingQuery();
-    const { data: dailyTasksProjectsQuery, isSuccess: isDailyTasksProjectsQuerySuccess } = useGetDailyTasksProjectsQuery();
-    const { data: chinguQuery, isSuccess: isChinguQuerySuccess } = useGetChinguQuery();
+  const {
+    data: routineActivityQuery,
+    isSuccess: isRoutineActivityQuerySuccess,
+  } = useGetRoutineActivitiesQuery();
+  const { data: studyingQuery, isSuccess: isStudyingQuerySuccess } =
+    useGetStudyingQuery();
+  const {
+    data: dailyTasksProjectsQuery,
+    isSuccess: isDailyTasksProjectsQuerySuccess,
+  } = useGetDailyTasksProjectsQuery();
+  const { data: chinguQuery, isSuccess: isChinguQuerySuccess } =
+    useGetChinguQuery();
 
-    let isDataSuccess = (!isRoutineActivityQuerySuccess || !isStudyingQuerySuccess || !isDailyTasksProjectsQuerySuccess || !isChinguQuerySuccess);
+  let isDataSuccess =
+    !isRoutineActivityQuerySuccess ||
+    !isStudyingQuerySuccess ||
+    !isDailyTasksProjectsQuerySuccess ||
+    !isChinguQuerySuccess;
 
-    return (
-        <aside>
-            <h3 className={styles['aside-menu-text']}>
-                Aside menu
-            </h3>
-            <section>
-                {isDataSuccess ? <p className={styles['dashboard-content-section__error']}>Data is not loaded yet</p> :
-                    (
-                        <>
-                            <ShowTasks apiEndPoint={routineActivityQuery} />
-                            <ShowTasks apiEndPoint={studyingQuery} />
-                            <ShowTasks apiEndPoint={dailyTasksProjectsQuery} />
-                            <ShowTasks apiEndPoint={chinguQuery} />
-                        </>
-                    )
-                }
-            </section>
-        </aside>
-    )
-}
+  return (
+    <aside>
+      <img src="/checklisLogo.svg" alt="Logo"></img>
+      <h3 className={styles["aside-menu-text"]}>Aside menu</h3>
+      <section>
+        {isDataSuccess ? (
+          <p className={styles["dashboard-content-section__error"]}>
+            Data is not loaded yet
+          </p>
+        ) : (
+          <>
+            <ShowTasks apiEndPoint={routineActivityQuery} />
+            <ShowTasks apiEndPoint={studyingQuery} />
+            <ShowTasks apiEndPoint={dailyTasksProjectsQuery} />
+            <ShowTasks apiEndPoint={chinguQuery} />
+          </>
+        )}
+      </section>
+    </aside>
+  );
+};
 
 export default SideMenu;
