dd759824516fa1af4f00d433caabd95553f994ea
Austine <austineuwumwonse@gmail.com>
2024-01-31 19:51:06 +0100

    navbar and sidemenu updated
    no functionality yet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,15 @@
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import SearchIcon from "@mui/icons-material/Search";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles["search-bar"]}>
-        <input type="text" placeholder="Search..." />
-        <button>
-          <SearchIcon />
-        </button>
+      <div>
+        <img
+          src="/checklisLogo.svg"
+          alt="Logo"
+          className={styles["checklis-icon"]}
+        ></img>
       </div>
-      <button className={styles["notifications-button"]}>
-        <NotificationsIcon />
-      </button>
     </header>
   );
 };
--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,6 +1,7 @@
 import styles from "./SideMenu.module.css";
-
+import Footer from "../Footer/Footer.jsx";
 import ShowTasks from "../ShowTasks/ShowTasks";
+import MyList from "../ListItems/ListItem.jsx";
 
 import {
   useGetRoutineActivitiesQuery,
@@ -31,22 +32,56 @@ const SideMenu = () => {
 
   return (
     <aside>
-      <img src="/checklisLogo.svg" alt="Logo"></img>
-      <h3 className={styles["aside-menu-text"]}>Aside menu</h3>
-      <section>
-        {isDataSuccess ? (
-          <p className={styles["dashboard-content-section__error"]}>
-            Data is not loaded yet
-          </p>
-        ) : (
-          <>
-            <ShowTasks apiEndPoint={routineActivityQuery} />
-            <ShowTasks apiEndPoint={studyingQuery} />
-            <ShowTasks apiEndPoint={dailyTasksProjectsQuery} />
-            <ShowTasks apiEndPoint={chinguQuery} />
-          </>
-        )}
-      </section>
+      <div className={styles["empty-container"]}></div>
+      <div className={styles["container"]}>
+        <button className={styles["container-button"]}>
+          <img
+            src="/dashboardIcon.svg"
+            alt="Logo"
+            className={styles["icon"]}
+          ></img>
+
+          <p>Dashboard</p>
+        </button>
+      </div>
+
+      <div className={styles["container"]}>
+        <button className={styles["container-button"]}>
+          <div className={styles["task-box"]}>
+            <img
+              src="/taskIcon.svg"
+              alt="Logo"
+              className={styles["icon"]}
+            ></img>
+            <p>Tasks</p>
+          </div>
+          <div className={styles["task-count"]}>
+            <p>16</p>
+          </div>
+        </button>
+      </div>
+      <div className={styles["empty-container"]}></div>
+      <MyList />
+      <div>
+        <section className={styles.apiContentSection}>
+          {isDataSuccess ? (
+            <p className={styles["dashboard-content-section__error"]}>
+              Data is not loaded yet
+            </p>
+          ) : (
+            <>
+              <ShowTasks apiEndPoint={routineActivityQuery} title="ROUTINE" />
+              <ShowTasks apiEndPoint={studyingQuery} title="STUDYING" />
+              <ShowTasks
+                apiEndPoint={dailyTasksProjectsQuery}
+                title="DAILY TASKS"
+              />
+              <ShowTasks apiEndPoint={chinguQuery} title="CHINGU" />
+            </>
+          )}
+        </section>
+      </div>
+      <Footer />
     </aside>
   );
 };
