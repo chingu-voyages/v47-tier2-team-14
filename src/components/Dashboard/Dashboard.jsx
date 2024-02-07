26ba01a98f886c0978b959c9d5d28f39e9252fac
Simon Summers <simon7summers@gmail.com>
2024-02-07 02:32:50 +0000

    calendar now dispalying tasks as events

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,17 +1,25 @@
 import styles from './Dashboard.module.css';
 import Todo from '../Todo/Todo';
+import { TaskCalendar } from "../Calendar/Calendar.jsx";
+import Card from "../Card/Card.jsx";
 
 const Dashboard = () => {
   return (
-    <main className={styles['dashboard-section']}>
-      <section className={styles['dashboard-heading-section']}>
-        <h2>Dashboard section</h2>
-      </section>
-      <section className={styles['dashboard-content-section']}>
-        <Todo />
-      </section>
-    </main>
-  )
+		<main className={styles["dashboard-section"]}>
+			<section className={styles["dashboard-heading-section"]}>
+				{/* <h2>Dashboard section</h2> */}
+        <Card title="routine activities"/>
+				<Card title="chingu"/>
+				<Card title="studying"/>
+        <Card title="daily tasks" />
+        <Card title="progress" />
+			</section>
+			<section className={styles["dashboard-content-section"]}>
+				<TaskCalendar />
+				{/* <Todo /> */}
+			</section>
+		</main>
+	);
 }
 
 export default Dashboard;
