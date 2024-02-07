869eda3fe715a93aab0c1404a4cb02853cbfdfff
Simon Summers <simon7summers@gmail.com>
2024-02-07 02:33:36 +0000

    previous commit was made without all files staged, calendar now displaying task events

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,176 +1,87 @@
+import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import taskData from "../../../assets/tasks-example.json";
+//import 'react-big-calendar/lib/sass/styles';
+import Tasks from "../../../assets/tasks-example.json";
 
 const localizer = momentLocalizer(moment);
+const tasks = Tasks;
 
-const myTasks = [
-	{
-		id: 0,
-		taskName: "Update Recipes Project Backlog",
-		taskDescription: "",
-		days: ["monday"],
-		activityName: "Projects",
-		categoryName: "ROUTINE ACTIVITIES",
-		allDay: true,
-		start: new Date(2024, 2, 5),
-		end: new Date(2024, 2, 6),
-	},
-	{
-		id: 1,
-		taskName: "Update The dailyTasks sheet with the backlog tasks",
-		taskDescription: "add the filtering feature to Done",
-		days: ["monday"],
-		activityName: "Projects",
-		categoryName: "ROUTINE ACTIVITIES",
-		allDay: true,
-	},
-	{
-		id: 2,
-		taskName: "Publish The recent Blog Post Draft to hashnode",
-		taskDescription: "",
-		days: ["friday"],
-		activityName: "Blog Posts",
-		categoryName: "ROUTINE ACTIVITIES",
-		allDay: true,
-	},
-	{
-		id: 3,
-		taskName: "Write a New headline in a Blog Post Draft",
-		taskDescription: "",
-		days: [
-			"saturday",
-			"sunday",
-			"monday",
-			"tuesday",
-			"wednesday",
-			"thursday",
-			"friday",
-		],
-		activityName: "Blog Posts",
-		categoryName: "ROUTINE ACTIVITIES",
-		allDay: true,
-	},
-	{
-		id: 4,
-		taskName: "Plan The Node Js Course Progress By Month",
-		taskDescription: "Set Up A Plan For The Next Month Of Node Js Learning",
-		days: ["1"],
-		activityName: "Node Js Course",
-		categoryName: "STUDYING",
-		allDay: true,
-	},
-	{
-		id: 5,
-		taskName: "Study The First Node Js Lecture",
-		taskDescription: "",
-		days: ["2"],
-		activityName: "Node Js Course",
-		categoryName: "STUDYING",
-		allDay: true,
-	},
-	{
-		id: 6,
-		taskName: "Plan The MongoDB Course Progress By Month",
-		taskDescription: "Set Up A Plan For The Next Month Of Node Js Learning",
-		days: ["30"],
-		activityName: "MongoDB",
-		categoryName: "STUDYING",
-		allDay: true,
-	},
-	{
-		id: 7,
-		taskName: "Study The First MongoDB Lecture",
-		taskDescription: "",
-		days: ["15"],
-		activityName: "MongoDB",
-		categoryName: "STUDYING",
-		allDay: true,
-	},
-	{
-		id: 8,
-		taskName: "Add The New Features list",
-		taskDescription: "",
-		days: ["monday"],
-		activityName: "Backlog",
-		categoryName: "DAILY TASKS PROJECT",
-		allDay: true,
-	},
-	{
-		id: 9,
-		taskName: "Add The New PRs To InReview",
-		taskDescription: "",
-		days: ["7"],
-		activityName: "Backlog",
-		categoryName: "DAILY TASKS PROJECT",
-		allDay: true,
-	},
-	{
-		id: 10,
-		taskName: "Work On The Sidebar",
-		taskDescription: "Add The Sections Links",
-		days: ["thursday"],
-		activityName: "Coding",
-		categoryName: "DAILY TASKS PROJECT",
-		allDay: true,
-	},
-	{
-		id: 11,
-		taskName: "Refactor The Filtering Feature Code",
-		taskDescription: "",
-		days: ["friday"],
-		activityName: "Coding",
-		categoryName: "DAILY TASKS PROJECT",
-		allDay: true,
-	},
-	{
-		id: 12,
-		taskName: "Conduct The Project Planning Meeting",
-		taskDescription:
-			"Conduct The Project Planning Meeting To Discuss Our Ideas",
-		days: ["monday"],
-		activityName: "Voyage",
-		categoryName: "CHINGU",
-		allDay: true,
-	},
-	{
-		id: 13,
-		taskName: "Create The UI/UX Design For The DailyTasks Project",
-		taskDescription:
-			"Create The UI/UX Design For The DailyTasks Project Based On The Team Discussion",
-		days: ["monday", "tuesday", "wednesday"],
-		activityName: "Voyage",
-		categoryName: "CHINGU",
-		allDay: true,
-	},
-	{
-		id: 14,
-		taskName: "Create When2Meet Link",
-		taskDescription: "Create When2Meet Link To Introduce Yourselves",
-		days: ["thursday"],
-		activityName: "Pair Programming",
-		categoryName: "CHINGU",
-		allDay: true,
-	},
-	{
-		id: 15,
-		taskName: "Attend The Introduction Meeting With Someone",
-		taskDescription: "",
-		days: ["thursday"],
-		activityName: "Pair Programming",
-		categoryName: "CHINGU",
-		allDay: true,
-	},
-];
-export const TaskCalendar = (props) => (
-	<div>
-		<Calendar
-			localizer={localizer}
-			events={myTasks}
-			startAccessor="start"
-			endAccessor="end"
-			style={{ height: 500 }}
-		/>
-	</div>
-);
+const TaskCalendar = () => {
+	// convert day strings to corresponding date objects
+	const getDayDate = (day) => {
+		if (typeof day === "string") {
+			return moment().day(day).toDate();
+		} else if (typeof day === "number") {
+			return moment().day(day).toDate();
+		}
+		return null;
+	};
+
+	// map tasks data to events that can be used by react-big-calendar
+	const events = tasks.flatMap((category) =>
+		category.activityTypes.flatMap((activityType) =>
+			activityType.Tasks.flatMap((task) =>
+				task.days.map((day) => ({
+					title: task.taskName,
+					start: getDayDate(day),
+					end: moment(getDayDate(day)).add(1, "day").toDate(),
+					desc: task.taskDescription,
+					categoryName: category.categoryName,
+					activityName: activityType.activityName,
+					isCompleted: false,
+				}))
+			)
+		)
+	);
+
+	console.log(events);
+
+	// add custom event styles
+	const categoryStyles = (event) => {
+		let style = {};
+
+		switch (event.categoryName) {
+			case "ROUTINE ACTIVITIES":
+				style = { backgroundColor: "#2EA194", color: "white" };
+                break;
+            
+			case "STUDYING":
+				style = { backgroundColor: "#FF981F", color: "white" };
+                break;
+            
+			case "DAILY TASKS PROJECT":
+				style = { backgroundColor: "#673AB3", color: "white" };
+                break;
+            
+			case "CHINGU":
+				style = { backgroundColor: "#EA1E61", color: "black" };
+                break;
+            
+			default:
+				break;
+		}
+
+		return {
+			style: style,
+		};
+    };
+    
+    
+
+	return (
+		<div style={{ height: 500 }}>
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				eventPropGetter={categoryStyles}
+				style={{ width: "70vw", margin: "50px" }}
+			/>
+		</div>
+	);
+};
+
+export default TaskCalendar;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import styles from './Card.module.css';
+import styles from "./Card.module.css";
 
-export default function Card(props) {
-  return (
-    <div className={styles["card"]}>{props.title}</div>
-  )
-}
+const Card = ({ title, completedTasks, totalTasks }) => {
+	return (
+		<div className={styles["card"]}>
+            <h4>{title}</h4>
+            <br/>
+            <h2>
+                {completedTasks}/{totalTasks}
+			</h2>
+		</div>
+	);
+};
+
+export default Card;
