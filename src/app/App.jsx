042110ba417df6926724bf0c90a288a4fd3bae08
Austine <austineuwumwonse@gmail.com>
2024-02-07 20:54:51 +0100

    Task Page and local storage added
    still on design. Local storage also added.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import "../App.css";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "../components/Header/Header";
 import SideMenu from "../components/SideMenu/SideMenu";
@@ -9,14 +10,22 @@ import Dashboard from "../components/Dashboard/Dashboard";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 
+import TaskPage from "../components/TaskPage/TaskPage";
+
 function App() {
   return (
     <Provider store={store}>
       <div className="app-container">
         <Header />
         <NavBar />
-        <SideMenu />
-        <Dashboard />
+
+        <Router>
+          <Routes>
+            <Route path="/" exact={<Dashboard />} />
+            <Route path="/taskpage" element={<TaskPage />} />
+          </Routes>
+          <SideMenu />
+        </Router>
         <Footer />
       </div>
     </Provider>
