3a72f342f0935e1b2ae1556f9b5fdc1c741d03d9
Mladen Krstin <mladen.krstin@gmail.com>
2024-02-08 17:26:04 +0100

    Created Modal component, edited TaskPane & SideBar

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,20 @@
+// import React, { useState } from 'react'
+import styles from './Modal.module.css'
+
+const Modal = ({ children, onClose }) => {
+	return (
+		<div className={styles.overlay}>
+			<div className={styles.modal}>
+				{children}
+				<button
+					className={styles.closeButton}
+					onClick={onClose}
+				>
+					X
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export default Modal
--- src/components/TaskPage/TaskPage.jsx
+++ src/components/TaskPage/TaskPage.jsx
@@ -1,58 +1,74 @@
-import styles from "../TaskPage/TaskPage.module.css";
-import { Link } from "react-router-dom";
+import styles from '../TaskPage/TaskPage.module.css'
+import Modal from '../Modal/Modal'
+import { useState } from 'react'
 
 const TaskPage = () => {
-  return (
-    <div className={styles.TaskPageContainer}>
-      <div className={styles.TaskPageNav}>
-        <div className={styles.TaskPageLeftNav}>
-          <div className={styles.TaskPageLeftNavDiv}>
-            <img
-              src="./rectangle-list.svg"
-              alt="Logo"
-              className={styles.TaskPageNavIcon}
-            ></img>
-            <p>List</p>
-          </div>
-          <div className={styles.TaskPageLeftNavDiv}>
-            <img
-              src="./calendar.svg"
-              alt="Logo"
-              className={styles.TaskPageNavIcon}
-            ></img>
-            <p>Calender</p>
-          </div>
-        </div>
+	const [isModalOpen, setModalOpen] = useState(false)
 
-        <Link className={styles.TaskPageNavRight}>
-          <button>
-            <img
-              src="./plus-small.svg"
-              alt="Logo"
-              className={styles.TaskPageNavIcon}
-            ></img>
-            <p> New Task</p>
-          </button>
-        </Link>
-      </div>
-      <div className={styles.TaskPageBody}>
-        <div className={styles.TaskPageBodyNav}>
-          <div className={styles.TaskPageBodyNav}>
-            <img
-              src="./angle-small-left.svg"
-              alt="Logo"
-              className={styles.TaskPageNavIcon}
-            ></img>
-            <p>Jan 01, 2024</p>
-            <img
-              src="./angle-small-right.svg"
-              alt="Logo"
-              className={styles.TaskPageNavIcon}
-            ></img>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-export default TaskPage;
+	return (
+		<div className={styles.TaskPageContainer}>
+			<div className={styles.TaskPageNav}>
+				<div className={styles.TaskPageLeftNav}>
+					<div className={styles.TaskPageLeftNavDiv}>
+						<img
+							src='./rectangle-list.svg'
+							alt='Logo'
+							className={styles.TaskPageNavIcon}
+						></img>
+						<p>List</p>
+					</div>
+					<div className={styles.TaskPageLeftNavDiv}>
+						<img
+							src='./calendar.svg'
+							alt='Logo'
+							className={styles.TaskPageNavIcon}
+						></img>
+						<p>Calender</p>
+					</div>
+				</div>
+
+				{/* NEW TASK BUTTON START */}
+				<div className={styles.TaskPageNavRight}>
+					<button onClick={() => setModalOpen(true)}>
+						<img
+							src='./plus-small.svg'
+							alt='Logo'
+							color='#fff'
+							className={styles.TaskPageNavIcon}
+						></img>
+						<p> New Task</p>
+					</button>
+
+					{isModalOpen && (
+						<Modal onClose={() => setModalOpen(false)}>
+							{/* MODAL BODY START */}
+							<h2>Modal Title</h2>
+							<p>This is the modal content!</p>
+							{/* MODAL BODY END */}
+						</Modal>
+					)}
+				</div>
+			</div>
+			{/* NEW TASK BUTTON END */}
+
+			<div className={styles.TaskPageBody}>
+				<div className={styles.TaskPageBodyNav}>
+					<div className={styles.TaskPageBodyNav}>
+						<img
+							src='./angle-small-left.svg'
+							alt='Logo'
+							className={styles.TaskPageNavIcon}
+						></img>
+						<p>Jan 01, 2024</p>
+						<img
+							src='./angle-small-right.svg'
+							alt='Logo'
+							className={styles.TaskPageNavIcon}
+						></img>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default TaskPage
