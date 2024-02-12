166c5dc4f82ec3df115b2f4f4505ceab8edc404c
Simon Summers <simon7summers@gmail.com>
2024-02-12 12:52:25 +0000

    calendar events are clickable

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from 'react';
+import Modal from "../Modal/Modal";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -8,8 +9,7 @@ import Tasks from "../../../assets/tasks-example.json";
 const localizer = momentLocalizer(moment);
 const tasks = Tasks;
 
-const TaskCalendar = () => {
-	// convert day strings to corresponding date objects
+// convert day strings to corresponding date objects
 	const getDayDate = (day) => {
 		if (typeof day === "string") {
 			return moment().day(day).toDate();
@@ -20,13 +20,13 @@ const TaskCalendar = () => {
 	};
 
 	// map tasks data to events that can be used by react-big-calendar
-	const events = tasks.flatMap((category) =>
+	const tasksToEvents = tasks.flatMap((category) =>
 		category.activityTypes.flatMap((activityType) =>
 			activityType.Tasks.flatMap((task) =>
 				task.days.map((day) => ({
 					title: task.taskName,
 					start: getDayDate(day),
-					end: moment(getDayDate(day)).add(1, "day").toDate(),
+					end: moment(getDayDate(day)),
 					desc: task.taskDescription,
 					categoryName: category.categoryName,
 					activityName: activityType.activityName,
@@ -36,6 +36,10 @@ const TaskCalendar = () => {
 		)
 	);
 
+const TaskCalendar = () => {
+	const [events, setEvents] = useState(tasksToEvents);
+	const [showModal, setShowModal] = useState(true)
+
 	console.log(events);
 
 	// add custom event styles
@@ -44,21 +48,21 @@ const TaskCalendar = () => {
 
 		switch (event.categoryName) {
 			case "ROUTINE ACTIVITIES":
-				style = { backgroundColor: "#2EA194", color: "white" };
-                break;
-            
+				style = { backgroundColor: "#2EA19410", color: "#2EA194" };
+				break;
+
 			case "STUDYING":
-				style = { backgroundColor: "#FF981F", color: "white" };
-                break;
-            
-			case "DAILY TASKS PROJECT":
-				style = { backgroundColor: "#673AB3", color: "white" };
-                break;
-            
+				style = { backgroundColor: "#FF981F10", color: "#FF981F" };
+				break;
+
+			case "DAILY TASKS":
+				style = { backgroundColor: "#673AB310", color: "#673AB3" };
+				break;
+
 			case "CHINGU":
-				style = { backgroundColor: "#EA1E61", color: "black" };
-                break;
-            
+				style = { backgroundColor: "#EA1E6110", color: "#EA1E61" };
+				break;
+
 			default:
 				break;
 		}
@@ -66,21 +70,47 @@ const TaskCalendar = () => {
 		return {
 			style: style,
 		};
-    };
-    
-    
+	};
+
+	const handleDoubleClick = (doubleClickedEvent) => {
+		const updatedEvent = {
+			...doubleClickedEvent,
+			isCompleted: !doubleClickedEvent.isCompleted,
+		};
+
+		const updatedEvents = events.map((event) =>
+			event === doubleClickedEvent ? updatedEvent : event
+		);
+
+		setEvents(updatedEvents);
+	};
+
+	const openModal = () => {
+		setShowModal(true);
+	};
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
 
 	return (
-		<div style={{ height: 500 }}>
-			<Calendar
-				localizer={localizer}
-				events={events}
-				startAccessor="start"
-				endAccessor="end"
-				eventPropGetter={categoryStyles}
-				style={{ width: "70vw", margin: "50px" }}
-			/>
-		</div>
+		<>
+			{showModal && <Modal onClose={closeModal}/>}
+			<div style={{ height: 650 }}>
+				<Calendar
+					localizer={localizer}
+					events={events}
+					startAccessor="start"
+					endAccessor="end"
+					eventPropGetter={categoryStyles}
+					style={{ width: "70vw", margin: "50px" }}
+					toolbar={false}
+					onSelectEvent={openModal}
+					onDoubleClickEvent={handleDoubleClick}
+					showAllEvents={true}
+				/>
+			</div>
+		</>
 	);
 };
 
